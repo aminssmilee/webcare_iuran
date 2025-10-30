de5b28abea6d5db0e5952caa83595d14b03ee343
aminssmilee <salisahmad48@gmail.com>
2025-10-30 14:02:40 +0700

    Upddate vite

--- vite.config.js
+++ vite.config.js
@@ -6,29 +6,9 @@ import path from 'path'
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/js/app.jsx'],
+      input: 'resources/js/app.jsx',
       refresh: true,
-      buildDirectory: 'build', // ✅ hasil build ke /public/build
     }),
     react(),
   ],
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'resources/js'),
-    },
-  },
-
-  build: {
-    outDir: 'public/build', // ✅ tempat hasil build (penting untuk deploy)
-    emptyOutDir: true,      // hapus file lama sebelum build baru
-    manifest: true,         // Laravel butuh file manifest.json
-    sourcemap: false,
-  },
-
-  server: {
-    host: '127.0.0.1', // ✅ aman di local dev
-    port: 5173,
-    strictPort: true,
-  },
 })
